852a79cfb89ef7b59661d1f7a48260637b8f1723
Павел Семенов <semenov.p.n@sbertech.ru>
2025-06-08 15:27:04 +0300

    added allure

--- src/test/java/com/practice/servicepractice/ServicePracticeApplicationTests.java
+++ src/test/java/com/practice/servicepractice/ServicePracticeApplicationTests.java
@@ -1,13 +1,13 @@
 package com.practice.servicepractice;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
-//class ServicePracticeApplicationTests {
-//
-//    @Test
-//    void contextLoads() {
-//    }
-//
-//}
+
+class ServicePracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+        Assertions.assertEquals(4 , 2 + 2);
+    }
+}
